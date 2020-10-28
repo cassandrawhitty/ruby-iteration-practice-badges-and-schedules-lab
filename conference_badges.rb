13380a1fc1e6a0514a34706fecf33a92b09b17d6
cassandrawhitty <cassandraallexwhitty@gmail.com>
2020-10-28 10:55:05 -0600

    commit tree c31790a35f18a293c830ef98a26b1f70f593cecf

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  return "Hello, my name is #{name}." 
+end
+
+def batch_badge_creator(names)  
+  list_of_names = names.map { |name| "Hello, my name is #{name}."}
+  return list_of_names
+end
+
+def assign_rooms(names)
+  names_w_indexes = Hash.new
+  names.each_with_index { |item, index| names_w_indexes[item] = index }
+  room_assignments = names_w_indexes.map { |name, index| "Hello, #{name}! You'll be assigned to room #{index +1}!" }
+  return room_assignments
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each { |item| puts item }
+  # binding.pry
+  assign_rooms(attendees).each { |item| puts item }
+end
+
